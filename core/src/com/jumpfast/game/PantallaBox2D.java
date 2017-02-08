1dd516d4e03adaea8f07565339d45175ed5d1049
Carlos Prieto <carlosprietotrabajo@gmail.com>
2017-02-08 20:14:32 +0100

    Añadido pincho

--- core/src/com/jumpfast/game/PantallaBox2D.java
+++ core/src/com/jumpfast/game/PantallaBox2D.java
@@ -24,8 +24,8 @@ public class PantallaBox2D extends PantallaInicio{
     //Creamos una camara 2D
     private OrthographicCamera camera;
 
-    private Body cuboBody;
-    private Fixture cuboFixture;
+    private Body cuboBody, sueloBody, pinchoBody;
+    private Fixture cuboFixture, sueloFixture, pinchoFixture;
 
     public PantallaBox2D(MainGame juego) {
         super(juego);
@@ -40,19 +40,59 @@ public class PantallaBox2D extends PantallaInicio{
         //Dubujar mundo por pantalla
         renderer = new Box2DDebugRenderer();
 
-        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        //camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera = new OrthographicCamera(7.11f, 4);
+        //movemos la camara 1m hacia arriba
+        camera.translate(0, 1);
 
-        BodyDef cuboDef = createCuboBodyDef();
-        cuboBody = world.createBody(cuboDef);
+        cuboBody = world.createBody(createCuboBodyDef());
+        sueloBody = world.createBody(createSueloBodyDef());
+        pinchoBody = world.createBody(createPinchoBodyDef(1));
 
         PolygonShape cuboShape = new PolygonShape();
         //se mide en metros
-        cuboShape.setAsBox(1, 1);
+        cuboShape.setAsBox(0.5f, 0.5f);
         cuboFixture = cuboBody.createFixture(cuboShape, 1);
         cuboShape.dispose();
 
+        PolygonShape sueloShape = new PolygonShape();
+        sueloShape.setAsBox(500, 1);
+        sueloFixture = sueloBody.createFixture(sueloShape, 1);
+        sueloShape.dispose();
+
+        //PolygonShape pinchoShape = new PolygonShape();
+        //pinchoShape.setAsBox(500, 1);
+        //pinchoFixture = pinchoBody.createFixture(pinchoShape, 1);
+        //pinchoShape.dispose();
+
+        pinchoFixture = createPinchoFixture(pinchoBody);
+
     }
 
+    private BodyDef createPinchoBodyDef(float x) {
+
+        BodyDef def = new BodyDef();
+        def.position.set(x, 0.5f);
+        return def;
+
+    }
+
+    /**
+     * Creamos el tipo suelo de forma estática
+     * @return
+     */
+    private BodyDef createSueloBodyDef() {
+
+        BodyDef def = new BodyDef();
+        def.position.set(0, -1);
+        return def;
+
+    }
+
+    /**
+     * Creamos el tipo cubo de forma dinamica
+     * @return
+     */
     private BodyDef createCuboBodyDef() {
 
         BodyDef def = new BodyDef();
@@ -62,12 +102,33 @@ public class PantallaBox2D extends PantallaInicio{
 
     }
 
+    private Fixture createPinchoFixture(Body pinchoBody){
+
+        Vector2[] vertices = new Vector2[3];
+        vertices[0] = new Vector2(-0.5f, -0.5f);
+        vertices[1] = new Vector2(0.5f, -0.5f);
+        vertices[2] = new Vector2(0, 0.5f);
+
+        PolygonShape shape = new PolygonShape();
+        shape.set(vertices);
+        Fixture fix = pinchoBody.createFixture(shape, 1);
+        shape.dispose();
+
+        return fix;
+
+    }
+
     @Override
     public void dispose() {
 
-        world.dispose();
         renderer.dispose();
+        world.dispose();
         world.destroyBody(cuboBody);
+        world.destroyBody(sueloBody);
+        world.destroyBody(pinchoBody);
+        cuboBody.destroyFixture(cuboFixture);
+        sueloBody.destroyFixture(sueloFixture);
+        pinchoBody.destroyFixture(pinchoFixture);
 
     }
 
